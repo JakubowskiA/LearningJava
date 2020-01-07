1a33ed896c3baf8af39c4f4d54cc112dc60304d3
Ariel Jakubowski <49914510+JakubowskiA@users.noreply.github.com>
2020-01-07 18:25:04 -0500

    Update LearningJava.java

--- LearningJava/LearningJava.java
+++ LearningJava/LearningJava.java
@@ -1,5 +1,17 @@
-// Test
+// CONDITIONALS
 
+// If else statement
+if (mood == “hungry”) {
+  System.out.println(“Eat something.”);
+}else if (mood == “tired”) {
+  System.out.println(“Take a nap.”);
+}else if (mood == “thirsty”) {
+  System.out.println(“Drink water.”);
+}else{
+System.out.println(“Pet your dog!”);
+}
+
+//Switch statement
 switch (mood) {
   case "hungry": 
    System.out.println(“Eat something.”);
