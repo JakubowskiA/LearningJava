b7d680d33213090d2fc4fbb777a511beb61923e0
Ariel Jakubowski <49914510+JakubowskiA@users.noreply.github.com>
2020-01-06 23:35:05 -0500

    Update LearningJava.java

--- LearningJava/LearningJava.java
+++ LearningJava/LearningJava.java
@@ -1,1 +1,16 @@
-Test
+// Test
+
+switch (mood) {
+  case "hungry": 
+   System.out.println(“Eat something.”);
+  break; 
+  case "tired": 
+     System.out.println(“Take a nap.”);
+  break; 
+    case "thirsty": 
+  System.out.println(“Drink water.”);
+  break; 
+    default:
+      System.out.println("Pet your dog.");
+}
+
